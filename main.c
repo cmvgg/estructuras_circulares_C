d046a93c29d30c2a033e6ae682a2fd0d56c4de4c
César Manuel Varela García <113544540+cmvgg@users.noreply.github.com>
2024-10-30 17:36:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,27 @@
 #include "circular_lists.h"
 
-int main() {
+int main(int argc, char **argv) {
     Node *stackA = NULL;
     Node *stackB = NULL;
 
+    if(argc <=1)
+        printf("inserte más valores por argumentos");
     printf("Insertando nodos en stackA:\n");
-    insertAtStart(&stackA, 10);
-    insertAtStart(&stackA, 20);
-    insertAtStart(&stackA, 30);
+  for (int i = 1; i < argc; i++) {
+        int value = atoi(argv[i]);
+        insertAtStart(&stackA, value);
+    }
     printList(stackA);
 
     printf("\nProbando swap en stackA:\n");
-    swap(&stackA);
+    swap(stackA);
     printList(stackA);
 
     printf("\nMoviendo nodo de stackA a stackB:\n");
     push(&stackA, &stackB);
+    printList(stackA);
+    printList(stackB);
+     push(&stackB, &stackA);
     printList(stackA);
     printList(stackB);
 
